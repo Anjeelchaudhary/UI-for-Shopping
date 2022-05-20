44d85f61c9c23c6981ac14036a2fe64196912899
anjeel chaudhary <chaudharyanjeel@gmail.com>
2022-05-20 06:03:38 +0545

    :art: added main HomePage
    Main Home Page is added

---
+++ lib/pages/Home/main_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainHomePage extends StatefulWidget {
+  MainHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MainHomePage> createState() => _MainHomePageState();
+}
+
+class _MainHomePageState extends State<MainHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
