9ef53c8b5c5f345c5a44bd1f4e89caa356a3afa4
anjeel chaudhary <chaudharyanjeel@gmail.com>
2022-05-23 06:32:43 +0545

    :sparkles: ReuseAble BigText is added
    Default size , color , font , and Text is added

--- lib/widgets/big_text.dart
+++ lib/widgets/big_text.dart
@@ -10,15 +10,29 @@ class BigText extends StatelessWidget {
 //Here const is removed because it does not  allows to hold the property dynamically
   BigText(
       {Key? key,
-      required this.color,
+      //Here I have written theis code Color(0xFF332d2b) because
+      //in order to make the default color we must write the exact naem of the color
+      this.color = const Color(0xFF332d2b),
       required this.text,
-      required this.size,
-      //Here the ellopsis means that if the Name extends more than given area then it will turn into dots and it is default property of flutter
+      this.size = 20,
+      //Here the ellopsis means that if the Name extends more than
+      //given area then it will turn into dots and it is default property of flutter
       this.overflow = TextOverflow.ellipsis})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Text(
+      //Here evry code defines for the default one if user has not given the
+      //custom requirements
+      text,
+      overflow: overflow,
+      style: TextStyle(
+        color: color,
+        fontSize: size,
+        fontFamily: 'Roboto',
+        fontWeight: FontWeight.w400,
+      ),
+    );
   }
 }
