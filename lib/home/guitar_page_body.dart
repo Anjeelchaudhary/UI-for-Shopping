678688c4b68123fde97544e704369bc3991269d9
anjeel chaudhary <chaudharyanjeel@gmail.com>
2022-05-24 16:26:36 +0545

    :memo: New file for body section
    initial code for the guitar page body

---
+++ lib/home/guitar_page_body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GuitarPageBody extends StatefulWidget {
+  GuitarPageBody({Key? key}) : super(key: key);
+
+  @override
+  State<GuitarPageBody> createState() => _GuitarPageBodyState();
+}
+
+class _GuitarPageBodyState extends State<GuitarPageBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
