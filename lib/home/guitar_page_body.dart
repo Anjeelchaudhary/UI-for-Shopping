1a9c6b4408b92fb85939924320ad69329fab155d
anjeel chaudhary <chaudharyanjeel@gmail.com>
2022-05-24 17:37:20 +0545

    :sparkles: Transion of widgets
    Starting code for transition of code is started

--- lib/home/guitar_page_body.dart
+++ lib/home/guitar_page_body.dart
@@ -10,6 +10,23 @@ class GuitarPageBody extends StatefulWidget {
 class _GuitarPageBodyState extends State<GuitarPageBody> {
   @override
   Widget build(BuildContext context) {
+    return Container(
+      //Here pageView.builder is for the transitioning of the
+      //diffenent wigits
+      child: PageView.builder(
+          itemCount: 5, //here 5 itemCount is default value
+          //itemBuilder takes function and no of itemCount and position is connectd to each other
+          //becuse the transition starts from zero to four
+          itemBuilder: (context, position) {
+            //_buildPageItem is used because I want something that scrolls left and right
+            //and it takes two item ie: position and
+            return _buildPageItem(position);
+          }),
+    );
+    //here we have to declare the function of _buildPageItem
+  }
+
+    Widget _buildPageItem(int index) {
     return Container();
   }
 }
